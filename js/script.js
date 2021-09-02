ddccf4395c16935023ad8619b720e0a5b01e41d7
Valentina-Fr <v_frasca@virgilio.it>
2021-09-02 14:12:29 +0200

    Print album cards

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    albums:{}
+  },
   methods: {},
+  created(){
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+      this.albums = res.data.response;
+    })
+  }
 });
