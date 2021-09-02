4688c913c0aaf1da3eb234b2ae6bad874c3912c9
Valentina-Fr <v_frasca@virgilio.it>
2021-09-02 15:24:18 +0200

    Filter genres

--- js/script.js
+++ js/script.js
@@ -5,14 +5,30 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-    albums:[]
+    albums:[],
+    musicGenre:"All"
   },
   computed: {
     yearSort(){
       return this.albums.sort((a, b) => {return a.year - b.year});
+    },
+    selectGenre(){
+      const genres = [];
+      this.yearSort.forEach(album => { 
+        if(!genres.includes(album.genre)){
+          genres.push(album.genre)
+        }
+      });
+      return genres;
     }
   },
-  methods: {},
+  methods: {
+    showGenre(album){
+      if(this.musicGenre === "All" || this.musicGenre === album.genre){
+        return true;
+      }
+    }
+  },
   created(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
       this.albums = res.data.response;
